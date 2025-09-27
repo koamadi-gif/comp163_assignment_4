dc87b9d1b1894133d2b269ddd4f5e022557f6ecc
koamadi-gif <koamadi@aggies.ncat.edu>
2025-09-26 21:52:44 -0400

    Update kobbyamadi_assignment_4.py

--- kobbyamadi_assignment_4.py
+++ kobbyamadi_assignment_4.py
@@ -46,6 +46,41 @@ print(f"Choose Course Load: {course_credits} credits")
 print(f"Suggestion: {recommendation}")
 
 
+complex_conditions = (not (current_gpa >= 3.8)) or (social_points < 50)
+
+if study_choice in study_options:
+    print(f"Study Option: {study_choice}")
+    complex_conditions = (not (current_gpa >= 3.8)) or (social_points < 50)
+    
+    if study_choice == "Programming":
+        if complex_conditions:
+            print("Your GPA isn't great and your social life isn't either. Perhaps do less partying and more studying.")
+            study_hours += 2
+        else: 
+            print("Your status is balanced. Now focus on improving your skills.")
+            
+    elif study_choice == "Math":
+        if study_choice == "Math" and current_gpa >= 3.8 and social_points >= 50: 
+            print("Good choice! Consider teach others your knowledge.")
+        else:
+            print("College math is way different from high school. Take time to actually study the material.")
+
+    elif study_choice == "English":
+        if not (stress_level >= 50): 
+            print("Great choice to balance your schedule. Remember to be creative.")
+        else:
+            print("Your putting too much stress on yourself.")
+            
+    elif study_choice == "History":
+        if current_gpa < 3.8 or study_hours < 8:
+            print("You need to read a lot more often. Try scheduling a reading time.")
+        else:
+            print("This class will give you good information. Try to be active in discussions.")
+
+    
+print(f"Updated Student Status:")
+print(f"Study Hours: {study_hours}")
+print(f"Complex Condition Check: {complex_conditions}")
 
 
 
